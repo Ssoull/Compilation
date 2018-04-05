2e3d9ede8258baf08ff76193647ec8b64c92e964
ssoull <gutierrezpablo33@gmail.com>
2018-04-05 10:58:23 +0200

    type

--- type.h
+++ type.h
@@ -1,18 +1,19 @@
 #ifndef TYPE_H_
 #define TYPE_H_
 
-enum Tag{INT,BOOL,CHAR,PTR,ARR,FUNC,PROD};
-struct Type{
-  enum Tag tag;
+typedef enum {INT, BOOL, CHAR, PTR, ARR, FUNC, PROD} Tag;
+
+typedef struct Type {
+  Tag tag;
   struct Type* left;
   struct Type* right;
   int min;
   int max;
-}
+} Type;
 
-struct Type* creaType(enum Tag,struct Type*,struct Type*);
-void displayType(struct Type*);
-int EqualTo(struct Type*,struct Type*);
+Type* createType(Tag tag, Type* left, Type* right);
+void displayType(Type* type);
+int EqualTo(Type* type1, Type* type2);
 
 
 #endif
